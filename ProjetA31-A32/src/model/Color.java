d9f923417911e78d8c5c2fc5180928715462979e
guillaume <none>
2023-11-24 15:02:55 +0100

    MAJ MVC

---
+++ ProjetA31-A32/src/model/Color.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum Color {
+    RED,GREEN,BLUE,YELLOW,ORANGE,PINK,GREY,WHITE
+}
--- ProjetA31-A32/src/model/MasterMindBoard.java
+++ ProjetA31-A32/src/model/MasterMindBoard.java
@@ -1,10 +1,25 @@
 package model;
 
+import java.awt.*;
+import java.util.Random;
+
 public class MasterMindBoard {
 
     private int lineCount;
     private int lineSize;
     private int currentLine = 0;
+    private Color[] secretCombination;
+    private Color[][] board;
+    public MasterMindBoard(int lineSize, int lineCount)
+    {
+        this.lineSize = lineSize;
+        this.lineCount = lineCount;
+
+        this.secretCombination = new Color[lineSize];
+        this.board = new Color[lineCount][lineSize];
+
+        GenerateSecretCombination();
+    }
 
     public void NextLine()
     {
@@ -13,5 +28,23 @@ public class MasterMindBoard {
     public void VerifyCurrentLine()
     {
 
+    }
+    public Color GetCellColor(int line, int column)
+    {
+        return this.board[line][column];
+    }
+    public void SetCellColor(Color color, int line, int column)
+    {
+        this.board[line][column] = color;
+    }
+    public void GenerateSecretCombination()
+    {
+        int value;
+        Random rand = new Random();
+        for(int i=0; i<this.lineSize; i++)
+        {
+            value = rand.nextInt(Color.values().length);
+            secretCombination[i] = Color.values()[value];
+        }
     }
 }
