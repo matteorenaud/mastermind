05bc359a460b5965b6278cd3c996d94229cd3964
guillaume <none>
2023-11-24 14:44:58 +0100

    Début MVC

---
+++ ProjetA31-A32/src/model/MasterMindBoard.java
@@ -0,0 +1,17 @@
+package model;
+
+public class MasterMindBoard {
+
+    private int lineCount;
+    private int lineSize;
+    private int currentLine = 0;
+
+    public void NextLine()
+    {
+        currentLine++;
+    }
+    public void VerifyCurrentLine()
+    {
+
+    }
+}
