6b6020f77f3977a08e109e17de8efee3fdfc28db
guillaume <none>
2023-11-28 08:58:23 +0100

    Création de la classe MasterMindLine

---
+++ ProjetA31-A32/src/model/MasterMindLine.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.util.ArrayList;
+
+public class MasterMindLine {
+
+    private int size; //Size of the line
+    private ArrayList<GameColor> cells; //Cells of the line
+    private int wellPlaced = 0; //Number of cells well-placed compared to the secret combination
+    private int wellChosen = 0; //Number of cells with a color present in the secret combination
+
+    public MasterMindLine(int size)
+    {
+        this.size = size;
+        this.cells = new ArrayList<GameColor>();
+    }
+
+    //Function that return true if the line is equal to the secret combination
+    //the function also fills the wellPLace and wellChosen attributes
+    public boolean verifyCurrentLine(MasterMindLine secretCombination)
+    {
+        this.wellPlaced = 0;
+        this.wellChosen = 0;
+
+        for(int i=0; i<this.size; i++)
+        {
+            if(this.getCellColor(i) == secretCombination.getCellColor(i))
+            {
+                this.wellPlaced ++;
+            }
+
+            if(this.cells.contains(secretCombination.getCellColor(i)))
+            {
+                this.wellChosen ++;
+            }
+        }
+
+        return this.wellPlaced == this.size;
+    }
+
+    //Function that gets the GameColor of a specific cell
+    public GameColor getCellColor(int index) throws ArrayIndexOutOfBoundsException
+    {
+        if(index >= size)
+        {
+            throw new ArrayIndexOutOfBoundsException("The size of the array is : " + this.size);
+        }
+
+        return this.cells.get(index);
+    }
+
+    //Function that sets the GameColor of a specific cell
+    public void setCellColor(GameColor color, int index) throws ArrayIndexOutOfBoundsException
+    {
+        if(index >= size)
+        {
+            throw new ArrayIndexOutOfBoundsException("The size of the array is : " + this.size);
+        }
+
+        this.cells.set(index,color);
+    }
+}
