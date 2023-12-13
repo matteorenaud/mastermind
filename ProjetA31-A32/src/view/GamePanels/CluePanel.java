2039273b29a7e9f7a365393a0de1e36bc4406ff3
behrg <none>
2023-12-13 12:07:12 +0100

    Fixed clues

--- ProjetA31-A32/src/view/GamePanels/CluePanel.java
+++ ProjetA31-A32/src/view/GamePanels/CluePanel.java
@@ -10,36 +10,25 @@ import java.util.ArrayList;
 public class CluePanel extends JPanel
 {
     private GameMasterController controller;
-    private JPanel pnlEasyClassicMode;
-    private JPanel pnlNumeric;
     private ArrayList<JLabel> clues;
-    private int lineCount;
 
     public CluePanel(int lineCount, int lineSize, GameMasterController controller)
     {
         this.controller = controller;
-
-        this.lineCount = lineCount;
-
-        this.pnlEasyClassicMode = new JPanel();
-        this.pnlEasyClassicMode.setLayout(new BoxLayout(pnlEasyClassicMode,BoxLayout.Y_AXIS));
-
-        this.pnlNumeric = new JPanel();
-        this.pnlNumeric.setLayout(new BoxLayout(pnlNumeric,BoxLayout.Y_AXIS));
+        this.setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
 
         this.clues = new ArrayList<JLabel>();
 
         for(int i=0;i<lineCount;i++)
         {
-            JLabel lbl  =new JLabel("");
+            JLabel lbl  = new JLabel("------------");
             lbl.setOpaque(true);
+            lbl.setSize(0,40);
+
             clues.add(lbl);
-            pnlNumeric.add(lbl);
-            pnlEasyClassicMode.add(lbl);
+            this.add(lbl);
         }
 
-
-        updateClues(lineCount-1);
 /*
         for(int i=0;i<lineCount;i++)
         {
@@ -55,29 +44,19 @@ public class CluePanel extends JPanel
             pnlEasyClassicMode.add(clue);
         }
         */
-
-        if(this.controller.getCurrentGameCluesMode() == CluesMode.NUMERIC_MODE)
-        {
-            this.add(pnlNumeric);
-        }
-        else
-        {
-            this.add(pnlEasyClassicMode);
-        }
     }
 
     public void updateClues(int line)
     {
         JLabel clue = this.clues.get(line);
 
-        clue.setText(
-                "Bien placé : "
-                        + this.controller.getCurrentLineWellPlaced()
-                        + "Mal placé : " +
-                        this.controller.getCurrentLineWellChosen());
-
-        pnlNumeric.add(clue);
+        if(this.controller.getCurrentGameCluesMode() == CluesMode.NUMERIC_MODE)
+        {
+            clue.setText(
+                    "Bien placé : "
+                            + this.controller.getCurrentLineWellPlaced()
+                            + "Mal placé : " +
+                            this.controller.getCurrentLineWellChosen());
+        }
     }
-
-
 }
